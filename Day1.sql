6b0094482becf2dce19888c20e6f49407f84ea62
Pooja <pooja016mca19@igdtuw.ac.in>
2020-09-19 01:27:49 +0530

    day1

---
+++ Day1.sql
@@ -0,0 +1,15 @@
+REM   Script: Day1
+REM   WireUsChallenge
+
+create table Student( 
+    rollNo int, 
+    name varchar(20), 
+    stream varchar(20), 
+    marks int 
+);
+
+insert into Student 
+values(0012020, 'Abc', 'Science', 98);
+
+select * from Student;
+
